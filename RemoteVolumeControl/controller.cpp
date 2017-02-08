840f0c7e1521f53c20d0ff754448ed467947c0d3
Роман Грозов <rmangrzv@gmail.com>
2017-02-08 18:59:38 +0300

    add parser

--- RemoteVolumeControl/controller.cpp
+++ RemoteVolumeControl/controller.cpp
@@ -1,4 +1,6 @@
 #include "SerialReadCOM.h"
+#include <cstring>
+
 
 void main() {
 	printf("Welcome to the serial test app!\n\n");
@@ -7,10 +9,13 @@ void main() {
 
 	if (SP->isConnected())
 		printf("Arduino Connected!");
-	
+
 	char incomingData[256] = "";
-	//char buttonPlus[11] = "FFA857\t\n";
-	//char buttonMinus[11] = "FFE01F\t\n";
+
+	char buttonPlus[11] = "FFA857";
+	char buttonMinus[11] = "FFE01F";
+	char delims[] = "\t\n";
+	char *context = nullptr;
 
 	int dataLenght = 255;
 	int readResult = 0;
@@ -25,11 +30,32 @@ void main() {
 		//else if(strcmp(incomingData, buttonMinus) == 0)
 		//	printf("-");
 		//else
-			printf("%s", incomingData);
+		printf("%s", incomingData);
 
-		
+
+		char *tok;
+
+		tok = strtok_s(incomingData, delims, &context);
+
+		while (tok){
+			printf("%s \n", tok);
+
+
+
+			if (strcmp(tok, buttonPlus) == 0)
+				printf("+\n\n");
+			else if (strcmp(tok, buttonMinus) == 0)
+				printf("-\n\n");
+
+			tok = strtok_s(nullptr, delims, &context);
+		}
+
+
+
 		Sleep(500);
 	}
-	
+
+
+
 	system("pause");
 }
