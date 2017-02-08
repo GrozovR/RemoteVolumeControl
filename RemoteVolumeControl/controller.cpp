f7606bbd03ea48818cc6b801a3be7c3c0fa038d8
Roman Grozov <rmangrzv@gmail.com>
2017-02-08 22:05:28 +0300

    not working version

--- RemoteVolumeControl/controller.cpp
+++ RemoteVolumeControl/controller.cpp
@@ -1,61 +1,98 @@
 #include "SerialReadCOM.h"
 #include <cstring>
+#pragma comment (lib, "winmm")
+
+const char buttonPlus[] = "FFA857";
+const char buttonMinus[] = "FFE01F";
+const char delims[] = "\r\n";
+const int dataLenght{ 255 };
+char incomingData[256] = "";
+char *context = nullptr;
+short volumeLevel{ 60 };
+
+
+struct mode 
+{
+	enum modes
+	{
+		VOLUMEUP_MODE,
+		VOLUMEDOWN_MODE,
+		IGNORE_MODE
+	} currentMode;
+} modeApp;
+
+
+void volumeUp()
+{
+	if (volumeLevel < 255)
+	{
+		volumeLevel++;
+		MMRESULT res = waveOutSetVolume(0, (DWORD)volumeLevel);
+	}
+}
+
+void volumeDown()
+{
+	if (volumeLevel >0){
+		volumeLevel--;
+		waveOutSetVolume(0, (DWORD)volumeLevel);
+	}	
+}
 
 
 void main() {
-	printf("Welcome to the serial test app!\n\n");
+	printf("Welcome to Remote Volume Control app!\n\n");
+	
+	//TODO: input port number
 
-	SerialReadCOM* SP = new SerialReadCOM("\\\\.\\COM4");
+	SerialReadCOM* SP = new SerialReadCOM("\\\\.\\COM3");
 
 	if (SP->isConnected())
 		printf("Arduino Connected!");
 
-	char incomingData[256] = "";
-
-	char buttonPlus[11] = "FFA857";
-	char buttonMinus[11] = "FFE01F";
-	char delims[] = "\t\n";
-	char *context = nullptr;
-
-	int dataLenght = 255;
-	int readResult = 0;
+	int readResult{ 0 };
+	modeApp.currentMode = mode::IGNORE_MODE;
 
 	while (SP->isConnected())
 	{
 		readResult = SP->ReadData(incomingData, dataLenght);
 		incomingData[readResult] = 0;
 
-		//if(strcmp(incomingData, buttonPlus) == 0)
-		//	printf("+");
-		//else if(strcmp(incomingData, buttonMinus) == 0)
-		//	printf("-");
-		//else
-		printf("%s", incomingData);
-
+		//printf("%s", incomingData);
 
 		char *tok;
-
 		tok = strtok_s(incomingData, delims, &context);
 
 		while (tok){
-			printf("%s \n", tok);
 
-
-
-			if (strcmp(tok, buttonPlus) == 0)
-				printf("+\n\n");
-			else if (strcmp(tok, buttonMinus) == 0)
-				printf("-\n\n");
+			if (strcmp(tok, buttonPlus) == 0) {
+				//printf("+\n\n");
+				modeApp.currentMode = mode::VOLUMEUP_MODE;
+				volumeUp();
+			}				
+			else if (strcmp(tok, buttonMinus) == 0) {
+				//printf("-\n\n");
+				modeApp.currentMode = mode::VOLUMEDOWN_MODE;
+				volumeDown();
+			}
+			else if(strcmp(tok,"FFFFFFFF") == 0){
+				//printf("%s\n", tok);
+				if (modeApp.currentMode == mode::IGNORE_MODE)
+					break;
+				else if (modeApp.currentMode == mode::VOLUMEUP_MODE)
+					volumeUp();
+				else if (modeApp.currentMode == mode::VOLUMEDOWN_MODE)
+					volumeDown();
+			}
+			else
+				modeApp.currentMode = mode::IGNORE_MODE;
 
 			tok = strtok_s(nullptr, delims, &context);
 		}
-
-
-
+		
 		Sleep(500);
 	}
 
-
-
+	
 	system("pause");
 }
